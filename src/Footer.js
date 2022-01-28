f8a754b0219e73af9027fc4b2c7c0db9bb647b51
Arye Eidelman <aryedov.e@gmail.com>
2022-01-28 15:41:00 -0500

    Install Tailwind 3

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ import styled from 'styled-components/macro'
 function Footer({ className }) {
   return (
     <footer className={className}>
-      <a href="https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces">
-        Chess Pieces </a> by <a href="https://en.wikipedia.org/wiki/User:Cburnett">
-        Colin M.L. Burnett </a> under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">
+      <a className="a-default" href="https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces">
+        Chess Pieces </a> by <a className="a-default" href="https://en.wikipedia.org/wiki/User:Cburnett">
+        Colin M.L. Burnett </a> under the <a className="a-default" href="http://creativecommons.org/licenses/by-sa/3.0/">
         CC BY-SA 3.0 </a> licence
     </footer>
   )
